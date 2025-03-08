5dedf71a1de4527614960910c3c71f588ef59154
backslash057 <gamedev035@gmail.com>
2025-03-08 01:24:40 +0100

    Updated database according to new requirements

--- database.sql
+++ database.sql
@@ -5,46 +5,29 @@ CREATE TABLE IF NOT EXISTS ClassroomUser(
   surnames VARCHAR(100) DEFAULT NULL,
   gender ENUM('M', 'F') DEFAULT NULL,
   birth_date DATE DEFAULT NULL,
+  role ENUM("STUDENT", "TEACHER") DEFAULT "STUDENT"
   password VARCHAR(256) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Course(
   course_id INT PRIMARY KEY AUTO_INCREMENT,
-  creator_id INT NOT NULL,
   creation_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   title VARCHAR(30) NOT NULL, 
   description VARCHAR(100) DEFAULT "",
-  code CHAR(6) NOT NULL,
-  CONSTRAINT FOREIGN KEY (creator_id) REFERENCES ClassroomUser(user_id)
+  code CHAR(6) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Chat(
-  chat_id INT PRIMARY KEY AUTO_INCREMENT,
-  title VARCHAR(30) NOT NULL,
-  creation_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  creator_id INT NOT NULL,
-  course_id INT NOT NULL,
-  CONSTRAINT FOREIGN KEY (creator_id) REFERENCES ClassroomUser(user_id),
-  CONSTRAINT FOREIGN KEY (course_id) REFERENCES Course(course_id)
-);
-
-CREATE TABLE IF NOT EXISTS Chat_join(
-  chat_join_id INT PRIMARY KEY AUTO_INCREMENT,
-  chat_id INT NOT NULL,
-  user_id INT NOT NULL,
-  join_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT FOREIGN KEY (user_id) REFERENCES ClassroomUser(user_id)
-);
 
 
 CREATE TABLE IF NOT EXISTS Message(
   message_id INT PRIMARY KEY AUTO_INCREMENT,
-  contenu TEXT NOT NULL,
-  heure_envoi DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  chat_id INT NOT NULL,
-  mentionned INT DEFAULT NULL, # can be NULL because message deleted
-  sender_id INT, # can be NULL because user can be deleted
-  CONSTRAINT FOREIGN KEY (chat_id) REFERENCES Chat(chat_id)
+  content TEXT NOT NULL,
+  send_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  course_id INT NOT NULL,
+  mention INT DEFAULT NULL, -- can be NULL because message deleted or no mention
+  sender_id INT, -- can be NULL because user can be deleted
+  deleted BOOLEAN  DEFAULT FALSE,
+  CONSTRAINT FOREIGN KEY (sender_id) REFERENCES Chat(ClassroomUser) ON DELETE SET NULL
 );
 
 
@@ -60,7 +43,7 @@ CREATE TABLE IF NOT EXISTS Course_inscription(
 CREATE TABLE IF NOT EXISTS Form(
   form_id INT PRIMARY KEY AUTO_INCREMENT,
   description VARCHAR(250),
-  data_path VARCHAR(100) NOT NULL
+  data_path VARCHAR(100) NOT NULL -- file to the associated json file
 );
 
 CREATE TABLE IF NOT EXISTS Assesment(
